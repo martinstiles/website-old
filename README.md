08609a15418459298ed6a4eac1a4a776de5234e2
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-01-27 22:12:25 +0100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# homepage
-My public webpage project. Currently under development.
---
+++ precache-manifest.9e161f3b2197602657ba8659489851d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fbd30e012f9cc657c59c283e1548bde",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "7e8b64fd8530f8e959b5",
+    "url": "/homepage/static/css/main.21287e4b.chunk.css"
+  },
+  {
+    "revision": "fecf32542194119c6f26",
+    "url": "/homepage/static/js/2.7df40eae.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/homepage/static/js/2.7df40eae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e8b64fd8530f8e959b5",
+    "url": "/homepage/static/js/main.ba65de04.chunk.js"
+  },
+  {
+    "revision": "d2aff02b19d17c103d02",
+    "url": "/homepage/static/js/runtime-main.4deff6df.js"
+  },
+  {
+    "revision": "9db088d50fa8b18de452997df7947a0d",
+    "url": "/homepage/static/media/Background.9db088d5.JPG"
+  }
+]);
