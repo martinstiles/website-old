0427173bcae2c5a61cdca11bba77085aa5856f30
Martin Stiles <martin.stiles@live.no>
2020-01-30 09:14:37 +0100

    Updates

---
+++ precache-manifest.e57bb4ecf71726e55037ad79820314ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb809cc4abd1151dc032c108e52916c",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "689dfc666aef134f2a71",
+    "url": "/website/static/css/main.2d2d9a3a.chunk.css"
+  },
+  {
+    "revision": "bcc2150b45e14362f64f",
+    "url": "/website/static/js/2.b0f74738.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/website/static/js/2.b0f74738.chunk.js.LICENSE"
+  },
+  {
+    "revision": "689dfc666aef134f2a71",
+    "url": "/website/static/js/main.e04f2889.chunk.js"
+  },
+  {
+    "revision": "65287229f92d9c18ac1c",
+    "url": "/website/static/js/runtime-main.6067079c.js"
+  },
+  {
+    "revision": "9db088d50fa8b18de452997df7947a0d",
+    "url": "/website/static/media/Background.9db088d5.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.5b2786219e2696fcc0e965bec62616d3.js"
+  "/website/precache-manifest.e57bb4ecf71726e55037ad79820314ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
