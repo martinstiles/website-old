00802dd015073daf181f08953c4866eaa15b79a1
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-02-02 23:45:05 +0100

    change opacity

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const imageStyle = {
     width: '100%',
     height: '100%',
-    opacity: '0.4',
+    opacity: '1',
     zIndex: 1,
     position: 'absolute',
     background: 'black'
