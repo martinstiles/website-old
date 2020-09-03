ae04f404c7ee9effa6e310c0a4ce12776503bc3a
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-09-03 20:21:13 +0200

    ref: rephrasing

--- src/views/about/components/experience/workExperienceIT.js
+++ src/views/about/components/experience/workExperienceIT.js
@@ -13,10 +13,10 @@ const WorkExperienceIT = () => {
         duration='January 2020 - July 2020'
         keywords='Key technologies: React, TypeScript, GraphQL, Git'
         description='Interning as a front end developer. Strise aims to revolutionize enterprise software, and being a part of
-          the team was a unique experience to me. I worked part time along side my studies from January til May, and then continued as
+          the team was a unique experience. I worked part time alongside my studies until May, and then continued as
           an intern during summer. My tasks naturally included a lot of bug squashing and refactoring, but also implementing new features 
-          in the product and creating internal tools for efficiency. I did, for instance, make a Slack bot to add previews for links to the
-          Strise product in slack, which you can read about in this blogpost I wrote (link will come as soon as it is released).'
+          and creating internal tools for efficiency. An example is making a Slack bot to add previews for links to the
+          Strise products. You can read about that and more in this blogpost I wrote for Strise (link will come as soon as it is released).'
       />
 
       <ExperienceBox
@@ -25,8 +25,8 @@ const WorkExperienceIT = () => {
         company='NTNU'
         duration='January 2020 - May 2020 '
         keywords='Keywords: Mentoring, SQL, ER diagrams, normalizing theory'
-        description='Student assistant in the course TDT4145: Data Modelling and Database Systems. Helping others professionally
-          turns out to be something I enjoy more than I imagined.'
+        description='Student assistant in the course TDT4145: Data Modelling and Database Systems.
+        It was a joy helping other students professionally.'
       />
     </InfoSection>
   )
